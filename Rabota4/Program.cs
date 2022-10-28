82ec21c196583862b011dfb871dce9fa76513eb2
Vladislav <vladperov@mail.ru>
2022-10-28 19:08:57 +0500

    sxema

---
+++ Rabota4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+int number=1;
+int N=16;
+while (number<=N)
+{
+    if(number % 2==0)
+    {
+    Console.WriteLine(number);
+    }
+    number+=1;
+}
