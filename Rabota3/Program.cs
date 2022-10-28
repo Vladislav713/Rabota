cb4ff4c1cc7c9db25556c038544fbf1e1aad53f3
Vladislav <vladperov@mail.ru>
2022-10-28 18:33:04 +0500

    gotovoe

---
+++ Rabota3/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+int a=4;
+int b=3;
+int c=7;
+if (a % 2 == 0)
+{
+    Console.WriteLine("a чётное");
+}
+else 
+{
+    Console.WriteLine("a нечётное");
+}
+if (b % 2 == 0)
+{
+    Console.WriteLine("b чётное");
+}
+else 
+{
+    Console.WriteLine("b нечётное");
+}
+if (c % 2 == 0)
+{
+    Console.WriteLine("c чётное");
+}
+else 
+{
+    Console.WriteLine("c нечётное");
+}
