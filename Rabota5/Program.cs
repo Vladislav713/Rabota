2279ed4aaf7346e152865168ef89502bf4351fce
Vladislav <vladperov@mail.ru>
2022-10-31 14:36:30 +0500

    rabota5

---
+++ Rabota5/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+num /= 10;
+num %= 10;
+Console.WriteLine(num);
+
+
