8f0b0c6884f63ad41fdca5edff0af4428febec4f
Vladislav <vladperov@mail.ru>
2022-10-31 15:07:20 +0500

    rabota6

---
+++ Rabota6/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int day=new Random().Next(1,8);
+
+if (day > 0 && day <= 5) 
+{
+    Console.WriteLine("Будний день "+ day);
+}
+if (day == 6 || day == 7) 
+{
+    Console.WriteLine("Выходной день "+ day);
+}
+
